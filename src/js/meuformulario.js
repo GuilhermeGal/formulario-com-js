e8e6bae3e43289b6725e6073bb5e00b81eeec0ad
GuilhermeGal <133656910+GuilhermeGal@users.noreply.github.com>
2024-10-13 16:34:45 -0300

    Add files via upload

---
+++ src/js/meuformulario.js
@@ -0,0 +1,18 @@
+function cadastro(){
+    const nome = document.getElementById('nome').value;
+    const idade = document.getElementById('idade').value;
+    const peso = document.getElementById('peso').value;
+    const sexo = document.getElementById('sexo').value;
+    console.log(nome);
+    
+    const dadosElement = document.getElementById('ixibirdados');
+    
+    dadosElement.innerHTML =
+    `nome: ${nome}
+    <br>
+    idade: ${idade}
+    <br>
+    peso: ${peso}
+    <br>
+    sexo: ${sexo}`;
+}
